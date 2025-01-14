7a2945cfc090242b331f8c2044969344cdee0949
Matthias <tuttiacc@hotmail.com>
2025-01-14 23:25:17 +0100

    added icon, bumped npm packages

--- ui/src/Avatar.svelte
+++ ui/src/Avatar.svelte
@@ -5,64 +5,79 @@
   import { getContext } from "svelte";
   import type { ZipTestStore } from "./store";
   import SvgIcon from "./SvgIcon.svelte";
+  import AboutDialog from "./AboutDialog.svelte";
+  import { EntryRecord } from "@holochain-open-dev/utils";
+  import { AsyncReadable } from "@holochain-open-dev/stores";
+  import { Profile } from "@holochain-open-dev/profiles";
 
-  const { getStore } :any = getContext("store");
+  const { getStore }: any = getContext("store");
   let store: ZipTestStore = getStore();
 
-  export let agentPubKey: AgentPubKey
-  export let size = 32
-  export let namePosition = "row"
-  export let showAvatar = true
-  export let showNickname = true
-  export let placeholder = false
-  export let disableAvatarPointerEvents = false
+  export let agentPubKey: AgentPubKey;
+  export let size = 32;
+  export let namePosition = "row";
+  export let showAvatar = true;
+  export let showNickname = true;
+  export let placeholder = false;
+  export let disableAvatarPointerEvents = false;
 
-  $: agentPubKey
-  $: agentPubKeyB64 = encodeHashToBase64(agentPubKey)
-  $: profile = store.profilesStore.profiles.get(agentPubKey)
-  $: nickname = $profile.status=="complete" && $profile.value ? $profile.value.entry.nickname : agentPubKeyB64.slice(5,9)+"..."
-  
+  $: agentPubKey;
+  $: agentPubKeyB64 = encodeHashToBase64(agentPubKey);
+  $: profile = store.profilesStore.profiles.get(agentPubKey) as AsyncReadable<
+    EntryRecord<Profile> | undefined
+  >;
+  $: nickname =
+    $profile.status == "complete" && $profile.value
+      ? $profile.value.entry.nickname
+      : agentPubKeyB64.slice(5, 9) + "...";
 </script>
-<div  class="avatar-{namePosition}"
-    title={showNickname ? "" : nickname}
-    >
-    {#if $profile.status == "pending"}
-        <sl-skeleton
-        effect="pulse"
-        style={`height: ${size}px; width: ${size}px;`}
-        ></sl-skeleton>
-    {:else if $profile.status == "complete"}
 
-        {#if showAvatar}
-            {#if placeholder && !$profile.value.entry.fields.avatar}
-                <SvgIcon icon=faUser size={`${size}`} style="margin-left:5px;margin-right:5px"/>
-            {:else}
-                <agent-avatar class:disable-ptr-events={disableAvatarPointerEvents} disable-tooltip={true} disable-copy={true} size={size} agent-pub-key="{agentPubKeyB64}"></agent-avatar>
-            {/if}
-        {/if}
-        {#if showNickname}
-            <div class="nickname">{ nickname }</div>
-        {/if}
+<div class="avatar-{namePosition}" title={showNickname ? "" : nickname}>
+  {#if $profile.status == "pending"}
+    <sl-skeleton effect="pulse" style={`height: ${size}px; width: ${size}px;`}
+    ></sl-skeleton>
+  {:else if $profile.status == "complete"}
+    {#if showAvatar}
+      {#if placeholder && !$profile.value.entry.fields.avatar}
+        <SvgIcon
+          icon="faUser"
+          size={`${size}`}
+          style="margin-left:5px;margin-right:5px"
+        />
+      {:else}
+        <agent-avatar
+          class:disable-ptr-events={disableAvatarPointerEvents}
+          disable-tooltip={true}
+          disable-copy={true}
+          {size}
+          agent-pub-key={agentPubKeyB64}
+        ></agent-avatar>
+      {/if}
     {/if}
+    {#if showNickname}
+      <div class="nickname">{nickname}</div>
+    {/if}
+  {/if}
 </div>
+
 <style>
-    .avatar-column {
-        align-items: center;
-        display:flex;
-        flex-direction: column;
-    }
-    .avatar-row {
-        display:inline-flex;
-        flex-direction: row;
-        justify-content:center;
-        position: relative;
-        height: 100%;
-        align-items: center;
-    }
-    .avatar-row .nickname{
-        margin-left: 0.5em;
-    }
-    .disable-ptr-events {
-        pointer-events:none;
-    }
+  .avatar-column {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+  }
+  .avatar-row {
+    display: inline-flex;
+    flex-direction: row;
+    justify-content: center;
+    position: relative;
+    height: 100%;
+    align-items: center;
+  }
+  .avatar-row .nickname {
+    margin-left: 0.5em;
+  }
+  .disable-ptr-events {
+    pointer-events: none;
+  }
 </style>
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { version, dnaVersion } from './package.json';  // Import version from package.json
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: resolve(__dirname, 'icon.png'),
+          dest: '.',
+        },
+      ]
+    })],
   server: {
     hmr: {
         host: 'localhost',
@@ -16,6 +26,6 @@ export default defineConfig({
   define: {
     '__APP_VERSION__': JSON.stringify(version),  // Define a global constant
     '__DNA_VERSION__': JSON.stringify(dnaVersion)  // Define a global constant
-  },
+  }
 });
 
