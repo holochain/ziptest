7f1577d9d89f2652dd35fbf2fc932c21b3132842
Eric Harris-Braun <eric@harris-braun.com>
2026-02-17 17:56:02 -0500

    refactor: complete rename - fishy/ to hwc/, GATEWAY_URL to LINKER_URL
    - Rename ui/src/fishy/ directory to ui/src/hwc/
    - Rename __GATEWAY_URL__ build constant to __LINKER_URL__
    - Rename build:fishy script to build:hwc
    - Update all Fishy comments to HWC/Holochain Web Conductor

--- ui/src/vite-env.d.ts
+++ ui/src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
 
-// Build-time constants for Fishy integration
-declare const __GATEWAY_URL__: string;
+// Build-time constants for Holochain Web Conductor integration
+declare const __LINKER_URL__: string;
 declare const __APP_VERSION__: string;
 declare const __DNA_VERSION__: string;
 
