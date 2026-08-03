9d61e3a9ae64c064f5c4980b2fe15d12798d33d5
Eric Harris-Braun <eric@harris-braun.com>
2026-08-03 16:22:58 -0400

    update to Holochain 0.7.0; bump versions UI 0.6.0-dev.0, DNA 0.3.0-dev.0
    New minor versions because the 0.7.0 DNA hash changes (not backward compatible).
    
    - crates: hdk =0.7.0, hdi =0.8.0; profiles dep pinned to tag v0.700.0
    - migrate integrity/coordinator zomes to the 0.7 FlatOp API
      (CreateEntry/Update/Delete/Link, ActionData, TypedAction)
    - flake.lock -> holochain-0.7.0
    - JS: @holochain/client ^0.21.0, @holochain-open-dev/* ^0.700.0,
      tryorama ^0.20.0, hc-spin ^0.700.0, @theweave/api+elements ^0.7.0-dev.1,
      @theweave/cli 0.16.0-dev.3

--- dnas/ziptest/zomes/coordinator/ziptest/src/lib.rs
+++ dnas/ziptest/zomes/coordinator/ziptest/src/lib.rs
@@ -80,8 +80,8 @@ pub fn post_commit(committed_actions: Vec<SignedActionHashed>) {
     }
 }
 fn signal_action(action: SignedActionHashed) -> ExternResult<()> {
-    match action.hashed.content.clone() {
-        Action::CreateLink(create_link) => {
+    match action.hashed.content.data.clone() {
+        ActionData::CreateLink(create_link) => {
             if let Ok(Some(link_type))
                 = LinkTypes::from_type(create_link.zome_index, create_link.link_type) {
                 emit_signal(Signal::LinkCreated {
@@ -91,7 +91,7 @@ fn signal_action(action: SignedActionHashed) -> ExternResult<()> {
             }
             Ok(())
         }
-        Action::DeleteLink(delete_link) => {
+        ActionData::DeleteLink(delete_link) => {
             let record = get(
                     delete_link.link_add_address.clone(),
                     GetOptions::default(),
@@ -102,8 +102,8 @@ fn signal_action(action: SignedActionHashed) -> ExternResult<()> {
                         .to_string())
                     ),
                 )?;
-            match record.action() {
-                Action::CreateLink(create_link) => {
+            match &record.action().data {
+                ActionData::CreateLink(create_link) => {
                     if let Ok(Some(link_type))
                         = LinkTypes::from_type(
                             create_link.zome_index,
@@ -125,7 +125,7 @@ fn signal_action(action: SignedActionHashed) -> ExternResult<()> {
                 }
             }
         }
-        Action::Create(_create) => {
+        ActionData::Create(_create) => {
             if let Ok(Some(app_entry)) = get_entry_for_action(&action.hashed.hash) {
                 emit_signal(Signal::EntryCreated {
                     action,
@@ -134,7 +134,7 @@ fn signal_action(action: SignedActionHashed) -> ExternResult<()> {
             }
             Ok(())
         }
-        Action::Update(update) => {
+        ActionData::Update(update) => {
             if let Ok(Some(app_entry)) = get_entry_for_action(&action.hashed.hash) {
                 if let Ok(Some(original_app_entry))
                     = get_entry_for_action(&update.original_action_address) {
@@ -147,7 +147,7 @@ fn signal_action(action: SignedActionHashed) -> ExternResult<()> {
             }
             Ok(())
         }
-        Action::Delete(delete) => {
+        ActionData::Delete(delete) => {
             if let Ok(Some(original_app_entry))
                 = get_entry_for_action(&delete.deletes_address) {
                 emit_signal(Signal::EntryDeleted {
--- dnas/ziptest/zomes/integrity/ziptest/src/thing.rs
+++ dnas/ziptest/zomes/integrity/ziptest/src/thing.rs
@@ -5,24 +5,22 @@ pub struct Thing {
     pub content: String,
 }
 pub fn validate_create_thing(
-    _action: EntryCreationAction,
+    _action: Action,
     _thing: Thing,
 ) -> ExternResult<ValidateCallbackResult> {
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_update_thing(
-    _action: Update,
+    _action: Action,
     _thing: Thing,
 ) -> ExternResult<ValidateCallbackResult> {
     Ok(ValidateCallbackResult::Valid)
 }
-pub fn validate_delete_thing(
-    _action: Delete,
-) -> ExternResult<ValidateCallbackResult> {
+pub fn validate_delete_thing(_action: Action) -> ExternResult<ValidateCallbackResult> {
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_create_link_thing_updates(
-    _action: CreateLink,
+    _action: Action,
     base_address: AnyLinkableHash,
     target_address: AnyLinkableHash,
     _tag: LinkTag,
@@ -33,39 +31,33 @@ pub fn validate_create_link_thing_updates(
         .entry()
         .to_app_option()
         .map_err(|e| wasm_error!(e))?
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest(String::from("Linked action must reference an entry"))
-            ),
-        )?;
+        .ok_or(wasm_error!(WasmErrorInner::Guest(String::from(
+            "Linked action must reference an entry"
+        ))))?;
     let action_hash = ActionHash::try_from(target_address).map_err(|err| wasm_error!(err))?;
     let record = must_get_valid_record(action_hash)?;
     let _thing: crate::Thing = record
         .entry()
         .to_app_option()
         .map_err(|e| wasm_error!(e))?
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest(String::from("Linked action must reference an entry"))
-            ),
-        )?;
+        .ok_or(wasm_error!(WasmErrorInner::Guest(String::from(
+            "Linked action must reference an entry"
+        ))))?;
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_delete_link_thing_updates(
-    _action: DeleteLink,
-    _original_action: CreateLink,
+    _action: Action,
+    _original_action: Action,
     _base: AnyLinkableHash,
     _target: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
-    Ok(
-        ValidateCallbackResult::Invalid(
-            String::from("ThingUpdates links cannot be deleted"),
-        ),
-    )
+    Ok(ValidateCallbackResult::Invalid(String::from(
+        "ThingUpdates links cannot be deleted",
+    )))
 }
 pub fn validate_create_link_all_things(
-    _action: CreateLink,
+    _action: Action,
     _base_address: AnyLinkableHash,
     target_address: AnyLinkableHash,
     _tag: LinkTag,
@@ -77,24 +69,20 @@ pub fn validate_create_link_all_things(
         .entry()
         .to_app_option()
         .map_err(|e| wasm_error!(e))?
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest(String::from("Linked action must reference an entry"))
-            ),
-        )?;
+        .ok_or(wasm_error!(WasmErrorInner::Guest(String::from(
+            "Linked action must reference an entry"
+        ))))?;
     // TODO: add the appropriate validation rules
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_delete_link_all_things(
-    _action: DeleteLink,
-    _original_action: CreateLink,
+    _action: Action,
+    _original_action: Action,
     _base: AnyLinkableHash,
     _target: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
-    Ok(
-        ValidateCallbackResult::Invalid(
-            String::from("AllThings links cannot be deleted"),
-        ),
-    )
+    Ok(ValidateCallbackResult::Invalid(String::from(
+        "AllThings links cannot be deleted",
+    )))
 }
